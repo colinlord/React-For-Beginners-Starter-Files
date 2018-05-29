a88a0bc0d248b2a1b219688c96e0b1dd340068a1
Colin Lord <colinlord@colinlord.com>
2018-05-29 18:26:46 -0400

    Video 4 Part 2

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,6 +4,7 @@ class StorePicker extends React.Component {
   render() {
     return (
       <React.Fragment>
+        { /* I'm a comment */}
         <h1>Hello!</h1>
         <p>I'm some content on multiple lines.</p>
       </React.Fragment>
